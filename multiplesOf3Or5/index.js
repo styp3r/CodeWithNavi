e114f6c9565a7ead8ecfc50caa5c805c28ec0f8c
Styp3r <stypertypes@gmail.com>
2021-08-16 00:23:26 +0530

    increase efficiency

--- multiplesOf3Or5/index.js
+++ multiplesOf3Or5/index.js
@@ -5,11 +5,10 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
-var [sum3, sum5] = [0,0];
+var sum =0;
 
 for(let i = 1; i <1000; i++){
-   i%3 == 0? (i%3 == i%5? null:sum3+= i): null;
-   i%5 == 0? (sum3+= i): null;
+   i%3 ==0 || i%5 == 0? sum += i: null;
 
 }
-console.log(sum3+sum5);
+console.log(sum);
